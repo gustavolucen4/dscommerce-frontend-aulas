30bdd8d2cc0c4c80805b2a6b71f3ea5811ef7738
Gustavo Lucena <gustavolucena12@gmail.com>
2023-03-14 17:24:21 -0300

    Searchbar onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import './style.css';
 
-export default function SearchBar() {
+type Props = {
+    onNewValue: Function
+}
+
+export default function SearchBar({ onNewValue }: Props) {
+
+    const [text, setText] = useState('');
+
+    function handleChange(event: any) {
+        setText(event.target.value)
+    }
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+        onNewValue(text);
+    }
+
+    function handleResetClick() {
+        setText('');
+        onNewValue(text);
+    }
 
     return (
-        <form className="dsc-search-bar">
+        <form onSubmit={handleSubmit} className="dsc-search-bar">
             <button type="submit">🔎︎</button>
-            <input type="text" placeholder="Nome do produto" />
-            <button type="reset">🗙</button>
+            <input value={text} type="text" placeholder="Nome do produto" onChange={handleChange} />
+            <button onClick={handleResetClick} className='dsc-btn-clear'>🗙</button>
         </form>
     );
 }
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -9,20 +9,24 @@ import { ProductDTO } from '../../../models/product';
 export default function Catalog() {
 
     const [products, setProducts] = useState<ProductDTO[]>([]);
+    const [productName, setProductName] = useState<string>('');
 
     useEffect(() => {
-
-        productService.findAll()
+        productService.findPageRequest(0, productName)
             .then(result => {
                 setProducts(result.data.content);
             })
-    }, [])
+    }, [productName])
+
+    function handleSearchSubmit(value: string) {
+        setProductName(value);
+    }
 
     return (
         <>
             <main>
                 <section id="catalog-section" className="dsc-container">
-                    <SearchBar />
+                    <SearchBar onNewValue={handleSearchSubmit} />
 
                     <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
                         {
