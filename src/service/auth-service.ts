c600b47caa8babf6219da17279125ceb16990dae
Gustavo Lucena <gustavolucena12@gmail.com>
2023-03-20 17:00:42 -0300

    has any hole

--- src/service/auth-service.ts
+++ src/service/auth-service.ts
@@ -1,9 +1,10 @@
 import { AxiosRequestConfig } from "axios";
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { requestBackend } from '../utils/requests'
 import * as accessTokenRepository from '../localStorage/access-token-repository'
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
 
@@ -14,7 +15,7 @@ export function loginRequest(loginData: CredentialsDTO) {
 
     const requestBody = QueryString.stringify({ ...loginData, grant_type: 'password' })
 
-    const config : AxiosRequestConfig = {
+    const config: AxiosRequestConfig = {
         method: 'POST',
         url: '/oauth/token',
         data: requestBody,
@@ -28,10 +29,40 @@ export function saveAccessToken(token: string) {
     accessTokenRepository.save(token);
 }
 
-export function getAccessToken (){
+export function getAccessToken() {
     return accessTokenRepository.get()
 }
 
 export function logout() {
     accessTokenRepository.remove();
+}
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+    try {
+        const token = accessTokenRepository.get();
+        return token == null ? undefined : (jwtDecode(token) as AccessTokenPayloadDTO);
+    } catch (error) {
+        return undefined;
+    }
+}
+
+export function isAuthenticated(): boolean {
+    let tokenPayload = getAccessTokenPayload();
+    return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+    if (roles.length === 0) {
+        return true;
+    }
+    const tokenPayload = getAccessTokenPayload();
+    if (tokenPayload !== undefined) {
+        for (var i = 0; i < roles.length; i++) {
+            if (tokenPayload.authorities.includes(roles[i])) {
+                return true;
+            }
+        }
+        //return roles.some(role => tokenData.authorities.includes(role));
+    }
+    return false;
 }
